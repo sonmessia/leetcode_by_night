8bae00576ebca78180aa978ed62e30081a480c85
HSonSDK <sonmessia123@gmai.com>
2025-09-20 12:41:56 +0700

    daily leetcode: Implement Router

---
+++ src/daily_problem/implement_router.rs
@@ -0,0 +1,85 @@
+use std::collections::{HashMap, VecDeque, HashSet};
+
+#[derive(Eq, PartialEq, Hash, Clone)]
+struct Packet {
+    source: i32,
+    destination: i32,
+    timestamp: i32,
+}
+
+struct Router {
+    packets: VecDeque<Packet>,
+    map: HashMap<i32, VecDeque<i32>>,
+    set: HashSet<Packet>,
+    limit: usize
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl Router {
+
+    fn new(memoryLimit: i32) -> Self {
+        Self {
+            packets: VecDeque::with_capacity(memoryLimit as usize),
+            map: HashMap::new(),
+            set: HashSet::new(),
+            limit: memoryLimit as usize
+        }
+    }
+
+    fn add_packet(&mut self, source: i32, destination: i32, timestamp: i32) -> bool {
+        let packet = Packet { source, destination, timestamp };
+        let ans = if !self.set.insert(packet.clone()) {
+            false
+        } else {
+            if self.packets.len() == self.limit {
+                self.pop_packet();
+            }
+
+            self.packets.push_back(packet.clone());
+            self.map.entry(destination).or_insert_with(VecDeque::new).push_back(timestamp);
+            true
+        };
+        ans
+    }
+
+    fn pop_packet(&mut self) -> Option<Packet> {
+        if let Some(packet) = self.packets.pop_front() {
+            self.set.remove(&packet);
+            self.map.entry(packet.destination).and_modify(|timestamps| {timestamps.pop_front();} );
+            Some(packet)
+        } else {
+            None
+        }
+    }
+
+    fn forward_packet(&mut self) -> Vec<i32> {
+        let res = if let Some(packet) = self.packets.pop_packet() {
+            vec![packet.source, packet.destination, packet.timestamp]
+        } else {
+            vec![]
+        };
+        res
+    }
+
+    fn get_count(&self, destination: i32, start_time: i32, end_time: i32) -> i32 {
+        if let Some(timestamps) = self.map.get(&destination) {
+            let p1 = timestamps.partition_point(|&x| x < start_time);
+            let p2 = timestamps.partition_point(|&x| x <= end_time);
+            (p2 - p1) as i32
+        } else {
+            0
+        }
+    }
+}
+
+/**
+ * Your Router object will be instantiated and called as such:
+ * let obj = Router::new(memoryLimit);
+ * let ret_1: bool = obj.add_packet(source, destination, timestamp);
+ * let ret_2: Vec<i32> = obj.forward_packet();
+ * let ret_3: i32 = obj.get_count(destination, startTime, endTime);
+ */
--- src/daily_problem/mod.rs
+++ src/daily_problem/mod.rs
@@ -10,6 +10,7 @@ pub mod find_most_frequent_vowel_and_consonant;
 pub mod find_n_unique_integers_sum_up_to_zero;
 pub mod find_the_number_of_ways_to_place_people;
 pub mod find_the_number_of_ways_to_place_people_ii;
+pub mod implement_router;
 pub mod maximum_area_of_longest_diagonal_rectangle;
 pub mod maximum_average_pass_ratio;
 pub mod maximum_number_of_words_you_can_type;
