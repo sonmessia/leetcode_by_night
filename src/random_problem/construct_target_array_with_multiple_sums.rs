74a86d3639571487c772cc88235404ada1876ca3
HSonSDK <sonmessia123@gmai.com>
2025-09-24 08:31:02 +0700

    random problem: Construct Target Array With Multiple Sums(Attempt)

---
+++ src/random_problem/construct_target_array_with_multiple_sums.rs
@@ -0,0 +1,26 @@
+struct Solution;
+
+impl Solution {
+    pub fn is_possible(target: Vec<i32>) -> bool {
+        use std::collections::BinaryHeap;
+        let mut heap = BinaryHeap::new();
+        let mut total: i64 = 0;
+        for &num in &target {
+            heap.push(num as i64);
+            total += num as i64;
+        }
+        while let Some(largest) = heap.pop() {
+            let rest = total - largest;
+            if largest == 1 || rest == 1 {
+                return true;
+            }
+            if rest == 0 || largest < rest || largest % rest == 0 {
+                return false;
+            }
+            let new_value = largest % rest;
+            total = rest + new_value;
+            heap.push(new_value);
+        }
+        true
+    }
+}
--- src/random_problem/mod.rs
+++ src/random_problem/mod.rs
@@ -1,6 +1,7 @@
 pub mod car_fleet;
 pub mod check_array_formation_through_concatenation;
 pub mod check_if_binary_string_has_at_most_one_segment_of_ones;
+pub mod construct_target_array_with_multiple_sums;
 pub mod count_nice_pairs_in_an_array;
 pub mod count_the_number_of_incremovable_subarrays_i;
 pub mod daily_temperatures;
