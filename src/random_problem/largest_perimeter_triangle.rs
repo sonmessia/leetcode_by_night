c24a6b5842fce261930ba675b32b6ed5d63d310c
HSonSDK <sonmessia123@gmai.com>
2025-09-27 10:22:43 +0700

    random problem: Largest Perimeter Triangle

---
+++ src/random_problem/largest_perimeter_triangle.rs
@@ -0,0 +1,14 @@
+struct Solution;
+
+impl Solution {
+    pub fn largest_perimeter(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        nums.sort_unstable_by(|a, b| b.cmp(a));
+        for i in 0..nums.len() - 2 {
+            if nums[i] < nums[i + 1] + nums[i + 2] {
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+        0
+    }
+}
--- src/random_problem/mod.rs
+++ src/random_problem/mod.rs
@@ -14,6 +14,7 @@ pub mod hash_divided_string;
 pub mod insert_delete_getrandom_o1_duplicates_allowed;
 pub mod intersection_of_multiple_arrays;
 pub mod k_diff_pairs_in_an_array;
+pub mod largest_perimeter_triangle;
 pub mod longest_non_decreasing_subarray_from_two_arrays;
 pub mod maximum_length_of_pair_chain;
 pub mod maximum_prime_difference;
