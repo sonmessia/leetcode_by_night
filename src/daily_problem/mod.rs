996ad9b6695e9a850512778aa047759d57cb5645
HSonSDK <sonmessia123@gmai.com>
2025-09-29 15:13:30 +0700

    daily leetcode: Minimum Score Triangulation of Polygon

--- src/daily_problem/mod.rs
+++ src/daily_problem/mod.rs
@@ -20,6 +20,7 @@ pub mod maximum_area_of_longest_diagonal_rectangle;
 pub mod maximum_average_pass_ratio;
 pub mod maximum_number_of_words_you_can_type;
 pub mod minimum_number_of_people_to_teach;
+pub mod minimum_score_triangulation_of_polygon;
 pub mod number_of_people_aware_of_a_secret;
 pub mod number_of_zero_filled_subarrays;
 pub mod replace_non_coprime_numbers_in_array;
