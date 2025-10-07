f03b351ad6760d43f84f270c6827c0c4656a39e4
HSonSDK <sonmessia123@gmai.com>
2025-10-07 10:54:17 +0700

    daily leetcode: Avoid Flood in The City

--- src/random_problem/find_the_safest_path_in_a_grid.rs
+++ src/random_problem/find_the_safest_path_in_a_grid.rs
@@ -1,9 +1,5 @@
 struct Solution;
 
 impl Solution {
-    pub fn maximum_safeness_factor(grid: Vec<Vec<i32>>) -> i32 {
-        if grid[0][0] == 1 || grid[grid.len() - 1][grid[0].len() - 1] == 1 {
-            return 0;
-        }
-    }
+    pub fn maximum_safeness_factor(grid: Vec<Vec<i32>>) -> i32 {}
 }
