55ca5dda0e4c0fefbae69d74401be869063b4f22
Nguyen Vo Hoang Son <sonmessia123@gmail.com>
2025-11-29 12:57:33 +0700

    daily leetcode: Minimum Operations to Make Array Sum Divisible by K

---
+++ src/daily_problem/minimum_operations_to_make_array_sum_divisible_by_k.rs
@@ -0,0 +1,11 @@
+struct Solution;
+
+impl Solution {
+    pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+        let total_sum: i32 = nums.iter().sum();
+        if total_sum % k != 0 {
+            return total_sum % k;
+        }
+        0
+    }
+}
