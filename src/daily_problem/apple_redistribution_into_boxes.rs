870dec251422a51d90866244931f8af63d2ebf47
HSonSDK <sonmessia123@gmai.com>
2025-12-24 10:34:09 +0700

    daily leetcode: Apple Redistribution into Boxes

---
+++ src/daily_problem/apple_redistribution_into_boxes.rs
@@ -0,0 +1,18 @@
+struct Solution;
+
+impl Solution {
+    pub fn minimum_boxes(apple: Vec<i32>, capacity: Vec<i32>) -> i32 {
+        let mut ans = 0;
+        let mut total = apple.iter().sum::<i32>();
+
+        capacity.sort_unstable_by(|a, b| b.cmp(a));
+
+        for i in capacity {
+            if total > 0 {
+                total -= i;
+                ans += 1;
+            }
+        }
+        ans
+    }
+}
--- src/daily_problem/mod.rs
+++ src/daily_problem/mod.rs
@@ -1,6 +1,7 @@
 pub mod adjacent_increasing_subarrays_detection_i;
 pub mod adjacent_increasing_subarrays_detection_ii;
 pub mod alice_and_bob_playing_flower_game;
+pub mod apple_redistribution_into_boxes;
 pub mod avoid_flood_in_the_city;
 pub mod best_time_to_buy_and_sell_stock_using_strategy;
 pub mod best_time_to_buy_and_sell_stock_v;
