11e7cc4cddc6ff9370ceb1242d6302b373f7580b
HSonSDK <sonmessia123@gmai.com>
2026-01-02 11:46:43 +0700

    random problem: Minimum Moves to Balance Circular Array

---
+++ src/random_problem/minimum_moves_to_balance_circular_array.rs
@@ -0,0 +1,43 @@
+struct Solution;
+
+impl Solution {
+    pub fn min_moves(balance: Vec<i32>) -> i32 {
+        let n = balance.len();
+        let mut j = -1;
+
+        for i in 0..n {
+            if balance[i] < 0 {
+                j = i as i32;
+            }
+        }
+
+        if j < 0 {
+            return 0;
+        }
+
+        if balance.iter().map(|&x| x as i64).sum::<i64>() < 0 {
+            return -1;
+        }
+
+        let mut ans: i64 = 0;
+        let mut d = 0;
+
+        let mut val = balance[j as usize];
+
+        while val < 0 {
+            d += 1;
+            let left = (j as usize + d) % n;
+            let right = (j as usize - d + n) % n;
+            let mut s = 0;
+            if left == right {
+                s = balance[left];
+            } else {
+                s = balance[left] + balance[right];
+            }
+            ans += s.min(-val) as i64 * d as i64;
+            val += s;
+        }
+
+        ans
+    }
+}
--- src/random_problem/mod.rs
+++ src/random_problem/mod.rs
@@ -44,6 +44,7 @@ pub mod maximum_total_damage_with_spell_casting;
 pub mod min_stack;
 pub mod minimum_amount_of_time_to_fill_cups;
 pub mod minimum_deletions_for_at_most_k_distinct_characters;
+pub mod minimum_moves_to_balance_circular_array;
 pub mod minimum_moves_to_equal_array_elements;
 pub mod minimum_number_of_operations_to_convert_time;
 pub mod most_popular_video_creator;
