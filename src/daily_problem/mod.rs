cc96d8fbddf286ef4f2e2ac0e4d136c6462d5a4a
HSonSDK <sonmessia123@gmai.com>
2026-01-18 11:44:29 +0700

    daily leetcode: Largest Magic Square

--- src/daily_problem/mod.rs
+++ src/daily_problem/mod.rs
@@ -58,6 +58,7 @@ pub mod greatest_sum_divisible_by_three;
 pub mod implement_router;
 pub mod increment_submatrices_by_one;
 pub mod keep_multiplying_found_values_by_two;
+pub mod largest_magic_square;
 pub mod largest_triangle_area;
 pub mod last_day_where_you_can_still_cross;
 pub mod lexicographically_smallest_string_after_applying_operations;
