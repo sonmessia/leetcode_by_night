255f1d5224cfa39ec2a687c4a7367bfa28501538
HSonSDK <sonmessia123@gmai.com>
2026-01-26 21:42:58 +0700

    random problem: Minimum Cost of Buying Candies With Discount

---
+++ src/daily_problem/minimum_cost_of_buying_candies_with_discount.rs
@@ -0,0 +1,17 @@
+struct Solution;
+
+impl Solution {
+    pub fn minimum_cost(cost: Vec<i32>) -> i32 {
+        let mut sorted_cost = cost;
+        sorted_cost.sort_unstable_by(|a, b| b.cmp(a));
+
+        let mut total_cost = 0;
+        for (i, &c) in sorted_cost.iter().enumerate() {
+            if (i + 1) % 3 != 0 {
+                total_cost += c;
+            }
+        }
+
+        total_cost
+    }
+}
