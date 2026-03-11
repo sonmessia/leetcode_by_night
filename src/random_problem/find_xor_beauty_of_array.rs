58c125345378a4ed96820f316df439e8cbe7ec39
Chicken_Yi <sonmessia123@gmai.com>
2026-03-11 20:48:23 +0700

    random problem: Find Xor-Beauty of Array

---
+++ src/random_problem/find_xor_beauty_of_array.rs
@@ -0,0 +1,8 @@
+struct Solution;
+
+impl Solution {
+    pub fn xor_beauty(nums: Vec<i32>) -> i32 {
+        nums.into_iter().fold(0, |acc, x| acc ^ x)
+    }
+}
+
--- src/random_problem/mod.rs
+++ src/random_problem/mod.rs
@@ -41,6 +41,7 @@ pub mod find_the_safest_path_in_a_grid;
 pub mod find_the_width_of_columns_of_a_grid;
 pub mod find_the_winning_player_in_coin_game;
 pub mod find_winner_on_a_tic_tac_toe_game;
+pub mod find_xor_beauty_of_array;
 pub mod frequency_of_the_most_frequent_element;
 pub mod hash_divided_string;
 pub mod insert_delete_getrandom_o1_duplicates_allowed;
