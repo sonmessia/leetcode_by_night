53c8226b8097a626d31e6ffbc60468642b4b5fa2
Chicken_Yi <sonmessia123@gmai.com>
2026-04-13 23:44:13 +0700

    daily leetcode: Minimum Distance to the Target Element

---
+++ src/daily_problem/minimum_distance_to_the_target_element.rs
@@ -0,0 +1,13 @@
+struct Solution;
+
+impl Solution {
+    pub fn get_min_distance(nums: Vec<i32>, target: i32, start: i32) -> i32 {
+        let mut ans = nums.len() as i32;
+        for i in 0..nums.len() {
+            if nums[i] == target {
+                ans = ans.min((i as i32 - start).abs());
+            }
+        }
+        ans
+    }
+}
