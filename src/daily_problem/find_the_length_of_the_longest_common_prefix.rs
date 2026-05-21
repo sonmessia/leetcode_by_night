cb79dd436fc6a5bc02e1a1f4bb7c6576f5ad1b34
Chicken_Yi <sonmessia123@gmai.com>
2026-05-21 12:35:28 +0700

    daily leetcode: Find the Length of the Longest Common Prefix

--- src/daily_problem/find_the_length_of_the_longest_common_prefix.rs
+++ src/daily_problem/find_the_length_of_the_longest_common_prefix.rs
@@ -1,21 +1,40 @@
+use std::collections::HashSet;
+
 struct Solution;
 
-#[derive(Default)]
-struct TrieNode {
-    children: [Option<Box<TrieNode>>; 10],
-    is_end_of_number: bool,
-}
-
-struct Trie {
-    root: TrieNode,
-}
-
-impl Trie {}
-
 impl Solution {
+    fn precompute_prefix(mut num: i32, set: &mut HashSet<i32>) {
+        while num > 0 {
+            set.insert(num);
+            num /= 10;
+        }
+    }
+
+    fn check_exist_prefix(num: i32, set: &HashSet<i32>) -> i32 {
+        let mut num = num;
+
+        let mut cnt = 0;
+        while num > 0 {
+            if set.contains(&num) {
+                cnt += 1;
+            }
+
+            num /= 10;
+        }
+
+        cnt
+    }
     pub fn longest_common_prefix(arr1: Vec<i32>, arr2: Vec<i32>) -> i32 {
+        let mut set: HashSet<i32> = HashSet::new();
         let mut ans = 0;
 
+        for num in arr1.into_iter() {
+            Self::precompute_prefix(num, &mut set);
+        }
+
+        for num in arr2.into_iter() {
+            ans = ans.max(Self::check_exist_prefix(num, &set));
+        }
         ans
     }
 }
